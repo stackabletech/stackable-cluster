e9543d076ea75b19c0cdd8755ca5b5e762f2a805
snocke <simon@nocke.eu>
2021-11-24 17:30:33 +0100

    initial application setup

---
+++ spark/minimalSpark/src/main/java/com/stackable/operator/minimal.java
@@ -0,0 +1,13 @@
+package com.stackable.operator;
+
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+public class minimal {
+    JavaSparkContext sparkContext = new JavaSparkContext(conf);
+
+
+
+
+
+}
