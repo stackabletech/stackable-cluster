bccb17dfe9409457f11f2aa5ecdfb31d7f7ca66b
snocke <simon@nocke.eu>
2022-01-18 17:27:29 +0100

    cleaning and restructure

--- kuttl-test/tests/spark/sparkAssertion.sh
+++ kuttl-test/tests/spark/sparkAssertion.sh
@@ -4,6 +4,8 @@ set -x
 
 REPO_DIR=$(dirname $0)
 
+echo ${REPO_DIR}
+
 export SPARK_MASTER_POD=$(kubectl get pods -o=name | grep master | sed "s/^.\{4\}//")
 echo $SPARK_MASTER_POD
 
@@ -11,12 +13,7 @@ export SPARK_SLAVE_POD=$(kubectl get pods -o=name | grep slave | sed "s/^.\{4\}/
 echo $SPARK_SLAVE_POD
 
 #.jar needs to be distributed to pods
-JAR_FILE="${REPO_DIR}/minimalSpark/build/libs/minimalSpark.jar"
-if [ ! -f ${JAR_FILE} ]; then
-    pushd ${REPO_DIR}
-    gradle build
-    popd
-fi
+JAR_FILE="${REPO_DIR}/minimalSpark/docker-gradle-build/minimalSpark.jar"
 
 kubectl cp ${JAR_FILE} $SPARK_MASTER_POD:/tmp
 kubectl cp ${JAR_FILE} $SPARK_SLAVE_POD:/tmp
@@ -31,5 +28,5 @@ kubectl cp ${RESOURCE_FILE} $SPARK_MASTER_POD:/tmp
 kubectl cp ${RESOURCE_FILE} $SPARK_SLAVE_POD:/tmp
 
 #make submit executable
-#kubectl exec --stdin $SPARK_MASTER_POD -- /bin/bash -c "chmod 700 /tmp/spark-submit.sh"
+kubectl exec --stdin $SPARK_MASTER_POD -- /bin/bash -c "chmod 700 /tmp/spark-submit.sh"
 kubectl exec $SPARK_MASTER_POD -- /bin/bash -x -v -c /tmp/spark-submit.sh &> spark-log.txt
--- startTests.sh
+++ startTests.sh
@@ -4,20 +4,21 @@ set -x
 
 REPO_DIR=$(dirname $0)
 
+echo ${REPO_DIR}
+
+# build image with openJDK 11 + gradle for constant jdk and .jar files
 docker image build --no-cache -t gradle-build:latest -f ${REPO_DIR}/kuttl-test/tests/spark/Dockerfile .
 docker container run -d --name jarBuilder -t gradle-build bash
 export CONTAINER_ID=$(docker ps -aqf "name=^jarBuilder$")
-
 docker exec -it ${CONTAINER_ID} bash -c "gradle build"
 
-docker cp ${CONTAINER_ID}:/stackable/minimalSpark/build/libs/minimalSpark.jar /tmp/
+mkdir ${REPO_DIR}/kuttl-test/tests/spark/minimalSpark/docker-gradle-build
+docker cp ${CONTAINER_ID}:/stackable/minimalSpark/build/libs/minimalSpark.jar ${REPO_DIR}/kuttl-test/tests/spark/minimalSpark/docker-gradle-build
+#chown 700 ${REPO_DIR}/kuttl-test/tests/spark/minimalSpark/docker-gradle-build/minimalSpark.jar
 
 docker stop ${CONTAINER_ID}
 docker rm ${CONTAINER_ID}
 
-#docker run -iv${PWD}:/stackable/minimalSpark/build/libs gradle-build sh -s <<EOF
-#chown -v $(id -u):$(id -g) minimalSpark.jar
-#cp -va minimalSpark.jar /tmp
-#EOF
+kubectl kuttl test -v 3
 
-#kubectl kuttl test -v 3
+rm -r -f ${REPO_DIR}/kuttl-test/tests/spark/minimalSpark/docker-gradle-build
