1661caabee70c91ebd700835a1b300170042c891
snocke <simon@nocke.eu>
2022-01-24 14:31:42 +0100

    more simplification

--- startTests.sh
+++ startTests.sh
@@ -6,10 +6,7 @@ REPO_DIR=$(dirname $0)
 
 echo ${REPO_DIR}
 
-# build image with openJDK 11 + gradle for constant jdk and .jar files
-docker image build -t jar-builder:latest -f ${REPO_DIR}/kuttl-test/tests/spark/Dockerfile .
-docker run --rm docker.stackable.tech/stackable/gradle:7.3.3-stackable0 \
-      -v $PWD/kuttl-test/tests/spark:/stackable:rw jar-builder build
+docker run --rm -v $PWD/kuttl-test/tests/spark:/spark/minimalSpark:rw -w /spark/minimalSpark gradle:7.3.3-jdk11 gradle clean build
 
 kubectl kuttl test -v 3
 
