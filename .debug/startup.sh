3e69d5d69ca4f554e584d8a02cdcd59b2f33b287
Simon Nocke <Simon@Nocke.eu>
2022-02-16 22:33:16 +0100

    Update startup.sh

--- .debug/startup.sh
+++ .debug/startup.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-export HOST_WORKSPACE='/Users/Simon/Repo/stackable/stackable-cluster'
+export HOST_WORKSPACE=$(pwd)
 export DOCKER_UID_GID="$(id -u):$(id -g)"
 export GIT_LOCAL_BRANCH="jenkins-setup"
 export REPO_DIR='/stackable-cluster'
@@ -15,7 +15,8 @@ docker run --rm \
     --volume "$HOST_WORKSPACE/target/:/target/" \
     --volume "$HOST_WORKSPACE/.ci/debug/cluster.yaml:/cluster.yaml" \
     --volume "$HOST_WORKSPACE/.ci/debug/test.sh:/test.sh" \
-    --volume "$HOST_WORKSPACE/kuttl-test/tests/spark/minimalSpark/distribution:/tmp" \
+    --volume "$HOST_WORKSPACE/kuttl-test.yaml:/kuttl-test.yaml" \
+    --volume "$HOST_WORKSPACE/kuttl-test:/kuttl-test" \
     --env T2_TOKEN=$T2_TOKEN \
     --env T2_URL=https://t2.stackable.tech \
     --env UID_GID="$DOCKER_UID_GID" \
@@ -23,9 +24,4 @@ docker run --rm \
     --env SPARK_OPERATOR_VERSION=NIGHTLY \
     --env GIT_BRANCH=$GIT_LOCAL_BRANCH \
     --env INTERACTIVE_MODE=true \
-    --env REPO_DIR=$REPO_DIR \
     docker.stackable.tech/t2-testdriver
-
-
-    # replace GIT branch in test script
-#    sed "s#\$GIT_BRANCH#$GIT_BRANCH#g" hdfs-operator/.ci/integration-tests/hcloud-centos-8/test.sh > _test.sh
