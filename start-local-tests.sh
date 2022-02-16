597997b7968356b664e8aef8f7dedcaecf893bc7
Simon Nocke <Simon@Nocke.eu>
2022-02-16 22:40:19 +0100

    Update start-local-tests.sh

--- start-local-tests.sh
+++ start-local-tests.sh
@@ -6,7 +6,7 @@ REPO_DIR=$(dirname $0)
 
 echo ${REPO_DIR}
 
-docker run --rm -v kuttl-test/tests/spark:/spark/minimalSpark:rw -w /spark/minimalSpark gradle:7.3.3-jdk11 gradle clean build
+docker run --rm -v $REPO_DIR/kuttl-test/tests/spark:/spark/minimalSpark:rw -w /spark/minimalSpark gradle:7.3.3-jdk11 gradle clean build
 
 kubectl kuttl test --kind-config ./kind.yaml --kind-context stkbl-cntxt
 
