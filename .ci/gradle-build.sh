178fdc2d46d00b6200e6028b8fd437e8f6487673
Simon Nocke <Simon@Nocke.eu>
2022-02-16 22:13:41 +0100

    Update .ci/gradle-build.sh
    Co-authored-by: Nikolaus Winter <mail@nikolaus-winter.de>

--- .ci/gradle-build.sh
+++ .ci/gradle-build.sh
@@ -2,10 +2,8 @@
 
 set -x
 
-HOST_WORKSPACE=${1}
+# The base directory in which this repo was checked out
+# As it may contain whitespaces, we take all params
+BASE_DIR=${@}
 
-docker run --rm -v $HOST_WORKSPACE/kuttl-test/tests/spark:/spark/minimalSpark:rw -w /spark/minimalSpark gradle:7.3.3-jdk11 gradle clean build
-
-exit_code=$?
-
-exit $exit_code
+docker run --rm -v "$BASE_DIR"/kuttl-test/tests/spark:/spark/minimalSpark:rw -w /spark/minimalSpark gradle:7.3.3-jdk11 gradle clean build
