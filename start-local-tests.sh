16741c88141bfa162132e4080a8340978b73bef1
snocke <simon@nocke.eu>
2022-02-28 16:58:47 +0100

    add absolute path to docker run, rework readme

--- start-local-tests.sh
+++ start-local-tests.sh
@@ -2,16 +2,9 @@
 
 set -x
 
-ENV=${1:-local}
+docker run --rm -v $(pwd)/kuttl-test/tests/spark:/spark/minimalSpark:rw -w /spark/minimalSpark gradle:7.3.3-jdk11 gradle clean build
 
-if [ "${ENV}" == "local" ]; then
-  REPO_DIR=$(pwd)
-  else
-  REPO_DIR=$(dirname ${0})
-fi
-echo ${REPO_DIR}
-
-docker run --rm -v "$REPO_DIR"/kuttl-test/tests/spark:/spark/minimalSpark:rw -w /spark/minimalSpark gradle:7.3.3-jdk11 gradle clean build
+./create_test_cluster.py --debug --kind --operator spark
 
 kubectl kuttl test -v 3
 
