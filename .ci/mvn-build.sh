963fe137740f20b5c9c95c327ada0409b77a7b47
snocke <simon@nocke.eu>
2022-04-12 14:40:22 +0200

    add user to docker build to enable cleanup for Jenkins-User

--- .ci/mvn-build.sh
+++ .ci/mvn-build.sh
@@ -5,6 +5,6 @@ set -x
 # The base directory in which this repo was checked out
 # As it may contain whitespaces, we take all params
 BASE_DIR=${@}
-id -u
+
+docker run -l "user=$(id -u)" --rm -v "$BASE_DIR"/test-jobs-root:/test-jobs-root -w /test-jobs-root maven:3.8.4-jdk-8 mvn clean install
 exit
-docker run --rm -v "$BASE_DIR"/test-jobs-root:/test-jobs-root -w /test-jobs-root maven:3.8.4-jdk-8 mvn clean install
