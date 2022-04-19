41b4049bdcf7f1c98ab5fa04ba6a816079c96d4c
snocke <simon@nocke.eu>
2022-04-19 11:49:23 +0200

    test user

--- start-local-tests.sh
+++ start-local-tests.sh
@@ -2,7 +2,9 @@
 
 set -x
 
-docker run --rm -v $(pwd)/test-jobs-root:/test-jobs-root -w /test-jobs-root maven:3.8.4-jdk-8 mvn clean install
+docker run -u $(id -u ${USER}):$(id -g ${USER}) \
+          --rm -v $(pwd)/test-jobs-root:/test-jobs-root \
+          -w /test-jobs-root maven:3.8.4-jdk-8 mvn clean install
 
 ./create_test_cluster.py --debug --kind --operator spark hbase zookeeper hdfs
 
