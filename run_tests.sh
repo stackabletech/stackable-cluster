737533302a0171bab3b56295b7be8396c5ee060e
snocke <simon@nocke.eu>
2022-05-05 10:36:39 +0200

    trigger jar build from run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-#docker run --rm -v $(pwd)/test-jobs-root:/test-jobs-root \
-#          -w /test-jobs-root maven:3.8.5-jdk-8 mvn clean install
-
 #./create_test_cluster.py --debug --kind --operator zookeeper spark hbase hdfs
+
+docker run --rm -v $(pwd)/test-jobs-root:/test-jobs-root \
+          -w /test-jobs-root maven:3.8.5-jdk-8 mvn clean install
 
 
 # Register absolute paths to pass to Ansible so the location of the role is irrelevant
