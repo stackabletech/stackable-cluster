7443b006e59b66105b81c6ff111f615ba9e210f6
snocke <simon@nocke.eu>
2022-05-16 16:53:22 +0200

    add logging and hdfs config

--- test-jobs-root/hbase/hbasePreparation.sh
+++ test-jobs-root/hbase/hbasePreparation.sh
@@ -15,6 +15,6 @@ JAR_FILE="${PROJECT_DIR}/hbase-1.0.jar"
 kubectl -n ${NAMESPACE} cp ${JAR_FILE} $HBASE_INTERACTIVE_POD:/tmp
 
 #start hbase job
-kubectl exec -n ${NAMESPACE} $HBASE_INTERACTIVE_POD -- /bin/bash -x -v -c "java -jar /tmp/hbase-1.0.jar" &> /tmp/hbase-log.txt
+kubectl exec -n ${NAMESPACE} $HBASE_INTERACTIVE_POD -- /bin/bash -x -v -c "java -jar /tmp/hbase-1.0.jar &>> /tmp/hbase-log.txt"
 
 
