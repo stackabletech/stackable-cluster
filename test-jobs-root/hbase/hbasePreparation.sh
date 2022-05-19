701d73ea665e1ae2fdf4893e6a5db7d5c24f2ef9
snocke <simon@nocke.eu>
2022-05-19 10:53:31 +0200

    add passing arguments and logging #2

--- test-jobs-root/hbase/hbasePreparation.sh
+++ test-jobs-root/hbase/hbasePreparation.sh
@@ -15,6 +15,16 @@ JAR_FILE="${PROJECT_DIR}/hbase-1.0.jar"
 kubectl -n ${NAMESPACE} cp ${JAR_FILE} $HBASE_INTERACTIVE_POD:/tmp
 
 #start hbase job
-kubectl exec -n ${NAMESPACE} $HBASE_INTERACTIVE_POD -- /bin/bash -x -v -c "java -jar /tmp/hbase-1.0.jar &>> /tmp/hbase-log.txt"
+# pass variables to job:
+# hbase-site-path: /stackable/conf/hbase/hbase-site.xml
+# hdfs-site-path: /stackable/conf/hdfs/hdfs-site.xml
+# core-site-path: /stackable/conf/hdfs/core-site.xml
+kubectl exec -n ${NAMESPACE} $HBASE_INTERACTIVE_POD -- /bin/bash -x -v -c "java -jar /tmp/hbase-1.0.jar \
+                                                                          --targetTable stackable \
+                                                                          --input /tmp/testdata.txt \
+                                                                          --hbaseSite /stackable/conf/hbase/hbase-site.xml \
+                                                                          --hdfsSite  /stackable/conf/hdfs/hdfs-site.xml \
+                                                                          --coreSite /stackable/conf/hdfs/core-site.xml \
+                                                                          &>> /tmp/hbase-log.txt"
 
 
