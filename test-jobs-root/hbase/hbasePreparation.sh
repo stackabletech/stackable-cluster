0d89b36417f562072d3ccad4f20c6402560d710f
snocke <simon@nocke.eu>
2022-05-30 17:11:28 +0200

    add upload to hdfs

--- test-jobs-root/hbase/hbasePreparation.sh
+++ test-jobs-root/hbase/hbasePreparation.sh
@@ -7,14 +7,25 @@ NAMESPACE=${1}
 
 echo ${PROJECT_DIR}
 
+# set env with name of HBASE interactive pod. This pod is used to communicate with the HBASE
 export HBASE_INTERACTIVE_POD=$(kubectl -n ${NAMESPACE} get pods -o=name | grep hbase-interactive | sed "s/^.\{4\}//")
 echo $HBASE_INTERACTIVE_POD
 
-#.jar needs to be distributed to pod
+# set env with name of HDFS Namenode. Needed to upload test data to HDFS
+export HDFS_NAMENODE=$(kubectl -n ${NAMESPACE} get pods -o=name | grep hdfs-namenode-default-0 | sed "s/^.\{4\}//")
+echo $HDFS_NAMENODE
+
+# .jar needs to be distributed to pod
 JAR_FILE="${PROJECT_DIR}/hbase-1.0.jar"
 kubectl -n ${NAMESPACE} cp ${JAR_FILE} $HBASE_INTERACTIVE_POD:/tmp
 
-#start hbase job
+# upload test data to hdfs
+RESOURCE="${PROJECT_DIR}/wine-dataset-small.txt"
+RESOURCE_FILE="wine-dataset-small.txt"
+kubectl -n ${NAMESPACE} cp ${RESOURCE} $HDFS_NAMENODE:/tmp
+kubectl exec -n ${NAMESPACE} $HDFS_NAMENODE -- /bin/bash -x -v -c "bin/hdfs dfs -put /tmp/${RESOURCE_FILE} /hbase"
+
+# start hbase job
 # pass variables to job:
 # hbase-site-path: /stackable/conf/hbase/hbase-site.xml
 # hdfs-site-path: /stackable/conf/hdfs/hdfs-site.xml
